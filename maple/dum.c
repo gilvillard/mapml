468e036e61c2ea666b88581aa48fa59ae5b4815b
Gilles Villard <gilles.villard@ens-lyon.fr>
2024-02-18 14:36:49 +0100

    Matrix conversions

--- maple/dum.c
+++ maple/dum.c
@@ -17,6 +17,8 @@
 
 // Todo variable x 
 
+// Voir où les initialisations 
+
 ALGEB to_maple_nmod_poly(MKernelVector kv, nmod_poly_t p){
 
     ALGEB maple_p; 
@@ -37,24 +39,48 @@ ALGEB to_maple_nmod_poly_mat(MKernelVector kv, nmod_poly_mat_t A){
 
     ALGEB maple_A; 
 
-    slong d = nmod_poly_degree(p);
+    slong m = A->r;
+    slong n = A->c;
 
-    maple_p = ToMapleInteger(kv,nmod_poly_get_coeff_ui(p, 0));
+    RTableSettings setting;
+    RTableGetDefaults(kv, &setting);
+    setting.num_dimensions=2;
+    setting.subtype=RTABLE_MATRIX;
+    
+    M_INT bounds[4];
+    bounds[0] = 1;
+    bounds[1] = m;
+    bounds[2] = 1;
+    bounds[3] = n;
 
-    ALGEB f = EvalMapleStatement(kv,"proc(pol,cc,dd) return (pol +cc*x^dd); end proc;");
+    maple_A = RTableCreate(kv,&setting,NULL,bounds);
 
-    for (slong i = 1; i <= d; i++) 
-        maple_p = EvalMapleProc(kv, f, 3, maple_p, ToMapleInteger(kv,nmod_poly_get_coeff_ui(p, i)),ToMapleInteger(kv,i));
 
-    return maple_p;   
-}
+    M_INT index[2];
+    RTableData tmp;
+
+    for (slong i=1; i<m+1; i++)
+        for (slong j=1; j<n+1; j++){
+
+            index[0]=i;
+            index[1]=j;
+
+            tmp.dag =  to_maple_nmod_poly(kv, nmod_poly_mat_entry(A,i-1,j-1));
+
+            RTableAssign(kv, maple_A, index, tmp);
+        }
+
+        return maple_A;
+
+    }
+
 
 
 // MapleToInteger64 vs slong and ulong 
 // From a maple list args[1], [... , degree, coeff...], args[2] = length/2, nb of monomials 
 // modulus assigned somewhere else 
 
-void get_nmod_poly_b(nmod_poly_t p, MKernelVector kv, ALGEB *args){
+    void get_nmod_poly_b(nmod_poly_t p, MKernelVector kv, ALGEB *args){
 
     ALGEB coeffs = args[1]; // Work with coeffs in input? 
 
@@ -65,16 +91,16 @@ void get_nmod_poly_b(nmod_poly_t p, MKernelVector kv, ALGEB *args){
     for (M_INT i=0; i<l; i++) {
 
         nmod_poly_set_coeff_ui(p, MapleToInteger64(kv,MapleListSelect(kv,coeffs,2*i+1)), 
-                               MapleToInteger64(kv,MapleListSelect(kv,coeffs,2*i+2)));
+           MapleToInteger64(kv,MapleListSelect(kv,coeffs,2*i+2)));
 
     }
 }
 
 // From a string at the flint format 
 
-void get_nmod_poly(nmod_poly_t p, MKernelVector kv, ALGEB *args){ 
+void get_nmod_poly(nmod_poly_t p, MKernelVector kv, ALGEB pol){ 
 
-    nmod_poly_set_str(p, MapleToString(kv,args[1]));
+    nmod_poly_set_str(p, MapleToString(kv,pol));
 
 }
 
@@ -85,7 +111,7 @@ ALGEB pget(MKernelVector kv, ALGEB *args){
 
     nmod_poly_init(p, 13);
 
-    get_nmod_poly(p, kv, args);
+    get_nmod_poly(p, kv, args[1]);
     
     return to_maple_nmod_poly(kv,p);
 
@@ -93,70 +119,49 @@ ALGEB pget(MKernelVector kv, ALGEB *args){
 
 
 
+// !!!!  Modulus
+
+void get_nmod_poly_mat(nmod_poly_mat_t A,   MKernelVector kv, ALGEB maple_A){
 
 
-ALGEB getmatrix(MKernelVector kv, ALGEB *args){
-
-
-    ALGEB A = args[1]; // Doesn't work with P in input? 
+    //ALGEB maple_A = args[1]; // Doesn't work with P in input? 
 
     M_INT m,n;   // slong flint or M_INT ? 
 
-    m = RTableUpperBound(kv, A, 1);
+    m = RTableUpperBound(kv, maple_A, 1);
 
-    n = RTableUpperBound(kv, A, 2);
+    n = RTableUpperBound(kv, maple_A, 2);
 
-    MapleALGEB_Printf(kv, " Rows %a columns %a",ToMapleInteger(kv,m),ToMapleInteger(kv,n));
+    nmod_poly_mat_init(A, m, n , 13);
 
+    M_INT index[2];
 
-    flint_rand_t state;
-    flint_randinit(state);
-    srand(time(NULL));
-    flint_randseed(state, rand(), rand());
+    RTableData tmp;             
 
-    nmod_poly_mat_t mat;
+    for (slong i=1; i<m+1; i++)
+        for (slong j=1; j<n+1; j++){
 
-    nmod_poly_mat_init(mat, m, n , 13);
+            index[0]=i;
+            index[1]=j;
+            tmp = RTableSelect(kv,maple_A,index);
 
-    nmod_poly_mat_rand(mat, state, 4);
+            nmod_poly_set_str(nmod_poly_mat_entry(A,i-1,j-1), MapleToString(kv,tmp.dag));
 
-    slong i=1,j=2;
-
-    nmod_poly_set_coeff_ui(mat->rows[i]+j, 0, 56);
-    nmod_poly_set_coeff_ui(mat->rows[i]+j, 3, 17);
-
-
-    nmod_poly_t res;
-    nmod_poly_init(res,mat->modulus);
-    nmod_poly_set(res,mat->rows[i]+j);
-
-    // ALGEB maplepol; 
-
-    // //--------------
-
-    // slong d = nmod_poly_degree(res);
-
-
-    // maplepol = ToMapleInteger(kv,nmod_poly_get_coeff_ui(res, 0));
-
-
-    // ALGEB f = EvalMapleStatement(kv,"proc(pol,cc,dd) return (pol +cc*x^dd); end proc;");
-
-    // for (slong i = 1; i <= d; i++) {
-
-    // maplepol = EvalMapleProc(kv, f, 3, maplepol, ToMapleInteger(kv,nmod_poly_get_coeff_ui(res, i)),ToMapleInteger(kv,i));
-
-
-    // }
-
-    return to_maple_nmod_poly(kv,res); 
+        }
 
 
 }
 
 
+ALGEB mget(MKernelVector kv, ALGEB *args){
 
+    nmod_poly_mat_t A;
 
+    get_nmod_poly_mat(A, kv, args[1]);
+    
+    return to_maple_nmod_poly_mat(kv,A);
+
+}
 
 
 /* -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
